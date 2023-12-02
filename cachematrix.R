6f653104997c638fdc9487da2337e35b04e4732f
Sonja Janssen-Sahebzad <123073089+SonjaJanssen@users.noreply.github.com>
2023-12-02 19:35:52 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -22,7 +22,7 @@ The goal of this script is to provide a mechanism for caching the inverse of a m
 ## C. Computing the inverse of a square matrix can be done with the solve function in R. 
 ##    For example, if X is a square invertible matrix, then solve(X) returns its inverse.
 ##    For this assignment, assume that the matrix supplied is always invertible.
-
+####-----------------------------------------------------------------------------------------------------------------------------------------------####
 Functions:
 1. makeCacheMatrix Function:
 Purpose: Creates a special "matrix" object that can cache its inverse.
